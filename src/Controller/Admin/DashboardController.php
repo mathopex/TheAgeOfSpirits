f6cdcbaf326613a1122a4455ce0c000c691e1821
mathopex <56123787+mathopex@users.noreply.github.com>
2022-01-10 16:51:42 +0100

    panel admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin')]
 class DashboardController extends AbstractController
 {
     #[Route('', name: 'admin_dashboard')]
@@ -15,15 +18,27 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/userGestion', name: 'user_gestion')]
-    public function UserGestion(): Response
+    public function UserGestion(UserRepository $userRepository,Request $request): Response
     {
-        return $this->render('admin/dashboard/index.html.twig');
+        if($q = $request->query->get('q'))
+        {
+            $users = $userRepository->search($q);
+        }
+        else
+        {
+            $users = $userRepository->findBy([],['id'=> 'DESC']);
+        }
+
+        
+        return $this->render('admin/user/index.html.twig',[
+            'users'=>$users,
+        ]);
     }
 
     #[Route('/contactGestion', name: 'contact_gestion')]
     public function ContactGestiton(): Response
     {
-        return $this->render('admin/dashboard/index.html.twig');
+        return $this->render('admin/contact/index.html.twig');
     }
 
 }
