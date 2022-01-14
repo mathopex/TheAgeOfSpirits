2d3e2376b8e13dcf0c8d9fb30bacb60cb44e66cb
Safir <safirnezz@live.fr>
2022-01-14 16:37:56 +0100

    Modification securité url admin et user et du css

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin')]
+#[Route('/m/admin')]
 class DashboardController extends AbstractController
 {
     #[Route('', name: 'admin_dashboard')]
--- src/Controller/User/DashboardController.php
+++ src/Controller/User/DashboardController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/m')]
 class DashboardController extends AbstractController
 {
     #[Route('/user/dashboard', name: 'user_dashboard')]
