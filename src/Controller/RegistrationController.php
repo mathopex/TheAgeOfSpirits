cb1742e4741efcea901cd52d2898c08344866f11
Safir <safirnezz@live.fr>
2022-01-14 17:57:23 +0100

    lien redirection

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -64,7 +64,7 @@ class RegistrationController extends AbstractController
             $notifyService->sendEmail($email);
 
             $this->addFlash('success', "Vous avez bien été inscris !");
-           return $this->redirectToRoute('perso');
+           return $this->redirectToRoute('register_confirmation');
 
        }
         return $this->render('registration/register.html.twig',
